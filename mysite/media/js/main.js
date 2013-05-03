13ed7cb1dc0c14e69d17e6d3c0b09ebbad458502
Ivan Gromov <summer.is.gone@gmail.com>
2013-05-04 00:27:40 +0600

    Create event by click

--- mysite/media/js/main.js
+++ mysite/media/js/main.js
@@ -1,3 +1,38 @@
 $(function() {
-    $("#calendar").fullCalendar({})
+    var calendar = $("#calendar").fullCalendar({
+        dayClick: function(date, allDay, jsEvent, view) {
+
+            var title = prompt("Enter event name"),
+                event_data = {
+                    title: title,
+                    start: date,
+                    duration: 60
+                };
+
+            $.ajax("/events/api/v1/event/", {
+                type: 'post',
+                data: JSON.stringify(event_data),
+                contentType: 'application/json'
+            })
+                .success(function() {
+                    calendar.fullCalendar('renderEvent', event_data);
+                })
+                .error(function(data) {
+                    var err = $.parseJSON(data.responseText);
+                    console.log(err.error_message);
+                    err.traceback.split('\n').forEach(function(line) {
+                        console.log(line);
+                    })
+
+                })
+
+        }
+    });
+
+    $.ajax("/events/api/v1/event/").success(function(data) {
+        data.objects.forEach(function(ev) {
+            calendar.fullCalendar('renderEvent', ev);
+        });
+
+    });
 });
